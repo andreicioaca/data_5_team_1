09dd47f1b7f8031ea2282bfe7fb4d58f3a6d169e
Agunbiade Peter <toluwalope00@gmail.com>
2023-01-30 11:29:31 +0100

    cleaned my workspace: task 2 is now divided into 2; using temp tables and using subquery

--- peter_input/northwind_db.sql
+++ peter_input/northwind_db.sql
@@ -18,8 +18,6 @@ ORDER BY 1
 --1-calculating the sales amount for the company 2016
 --2017-2018 using order and order details tables and seperate the sales amounts for 
 --3 categories low-medium-high team leader will decide the threshold of each category according to the Normal distribution of the sales
-
-
 SELECT
     od.*,
     (od.UnitPrice - od.Discount * od.UnitPrice) * od.Quantity AS total_sales
@@ -27,9 +25,8 @@ FROM
     "Order Details" od
     JOIN Orders o ON od.OrderID = o.OrderID
 
+
 --Using subquery I created a query to get the to total sales and the total discounting then subtrated the discount for the total sales to get the actual_sales
-
-
 SELECT
     order_date,
     ordered_country,
@@ -49,9 +46,8 @@ FROM
     )
 ORDER BY actual_sales 
 
--- I will now create a temp table as sales report with the imformation recovered from the first qwery
 
-
+--created a temp table as sales_report with the imformation recovered from the subquery
 DROP TABLE IF EXIST 'sales_report';
 CREATE TEMP TABLE 'sales_report' AS
 SELECT
@@ -72,21 +68,9 @@ FROM
             JOIN Orders o ON od."OrderID" = o."OrderID"
     )
     
-    
---Calling the table just created 
-SELECT
-    *
-FROM
-    sales_report
 
 --the order_date table cant the use easily with the way it is formatted (since we only need sales based on 2016,2017 and 2018) from all the years in the database
 -- the order_date column will be formatted yearly using STRFTIME FUNCTION
-
-SELECT
-    strftime('%Y', order_date) as year,
-    *
-FROM
-    sales_report
 
 
 --Create a temp Table  yearly_sales_report
@@ -103,26 +87,57 @@ FROM
     sales_report
     
     
---caliing the just created table
-SELECT
-    *
-FROM
-    yearly_sales_report
-
-
---next step ranking low sales region, medium sales region and high sales region (Using percentile ranking)
-
---High sales	Medium sales			Low sales
---Above 84%		Between 16% and 84%		Below 16%
---$936.7		$92.4 - $936.7				$92.4
+-- FINAL RESULT FROM THE TABLES THAT WERE CREATED
+--High sales		Medium sales				Low sales
+--Above 84%			Between 84% and 16%			Below 16%
+--(38105.675)		(38105.675 - 2691.7)		(2691.7)
 
 SELECT
-    *,
+    year,
+    ordered_country,
+    SUM(actual_sales),
     CASE
-        WHEN actual_sales >= 936.7 THEN 'high sales'
-        WHEN actual_sales < 936.7
-        AND actual_sales > 92.4 THEN 'medium sales'
-        WHEN actual_sales < 92.4 THEN 'low sales'
+        WHEN SUM(actual_sales) >= 38105.675 THEN 'high sales'
+        WHEN SUM(actual_sales) < 38105.675
+        AND SUM(actual_sales) >= 2691.7 THEN 'medium sales'
+        WHEN SUM(actual_sales) < 2691.7 THEN 'low sales'
     END AS 'sales_rank'
 FROM
     yearly_sales_report
+GROUP BY 1, 2
+
+
+
+
+--USING SUBQERY TASK 2 
+
+--High sales		Medium sales				Low sales
+--Above 84%			Between 84% and 16%			Below 16%
+--(38105.675)		(38105.675 - 2691.7)		(2691.7)
+
+SELECT
+    strftime('%Y', order_date) as year,
+    ordered_country,
+    SUM(sales - total_discount) as actual_sales,
+        CASE
+        WHEN SUM(sales - total_discount) >= 38105.675 THEN 'high sales'
+        WHEN SUM(sales - total_discount) < 38105.675
+        AND SUM(sales - total_discount) >= 2691.7 THEN 'medium sales'
+        WHEN SUM(sales - total_discount) < 2691.7 THEN 'low sales'
+    END AS 'sales_rank'
+FROM
+    (
+        SELECT
+            o."OrderDate" order_date,
+            o."ShipCountry" ordered_country,
+            (od."UnitPrice" * od."Quantity") sales,
+            (od."UnitPrice" * od."Quantity") * od."Discount" as total_discount
+        FROM
+            "Order Details" od
+            JOIN Orders o ON od."OrderID" = o."OrderID"
+    )
+GROUP BY
+    1,
+    2
+ORDER BY
+    1 
--- peter_input/readme.txt
+++ peter_input/readme.txt
@@ -1,7 +1,41 @@
 task for week 1:
 What countries does the company sell its products to and what are the categories sold to each country? 
+-- Used group_concat function to ensure all the categories are listed
 
 
 task for week 2:
 calculating the sales amount for the company 2016
 2017-2018 using order and order details tables and seperate the sales amounts for 3 categories low-medium-high team leader will decide the threshold of each category according to the Normal distribution of the sales
+Using percentile ranking 
+
+--High sales		Medium sales				Low sales
+--Above 84%		Between 84% and 16%			Below 16%
+--(38105.675)		(38105.675 - 2691.7)			(2691.7)
+
+
+SELECT
+    strftime('%Y', order_date) as year,
+    ordered_country,
+    SUM(sales - total_discount) as actual_sales,
+        CASE
+        WHEN SUM(sales - total_discount) >= 38105.675 THEN 'high sales'
+        WHEN SUM(sales - total_discount) < 38105.675
+        AND SUM(sales - total_discount) >= 2691.7 THEN 'medium sales'
+        WHEN SUM(sales - total_discount) < 2691.7 THEN 'low sales'
+    END AS 'sales_rank'
+FROM
+    (
+        SELECT
+            o."OrderDate" order_date,
+            o."ShipCountry" ordered_country,
+            (od."UnitPrice" * od."Quantity") sales,
+            (od."UnitPrice" * od."Quantity") * od."Discount" as total_discount
+        FROM
+            "Order Details" od
+            JOIN Orders o ON od."OrderID" = o."OrderID"
+    )
+GROUP BY
+    1,
+    2
+ORDER BY
+    1 
